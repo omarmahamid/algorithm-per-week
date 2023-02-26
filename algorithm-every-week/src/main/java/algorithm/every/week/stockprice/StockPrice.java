f6f25665f482fef422624762e03eb058755cfa67
omar.mahamid <omar.taiseerr@gmail.com>
2023-02-26 20:31:55 +0200

    S-0003

---
+++ algorithm-every-week/src/main/java/algorithm/every/week/stockprice/StockPrice.java
@@ -0,0 +1,25 @@
+package algorithm.every.week.stockprice;
+
+public class StockPrice{
+
+    public StockPrice() {
+        
+    }
+
+    public void update(int timestamp, int price) {
+
+    }
+
+    public int current() {
+
+    }
+
+    public int maximum() {
+
+    }
+
+    public int minimum() {
+
+    }
+
+}
